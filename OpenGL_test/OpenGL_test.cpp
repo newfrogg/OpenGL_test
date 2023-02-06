5bf64ae068f67fb2ded4e6633fd38cffe87732d6
newfrogg <85973867+newfrogg@users.noreply.github.com>
2023-02-06 15:15:43 +0700

    simple OpenGL program template

--- OpenGL_test/OpenGL_test.cpp
+++ OpenGL_test/OpenGL_test.cpp
@@ -5,20 +5,39 @@
 #include <GL/glut.h>
 
 using namespace std;
+void init() {
+    return;
+}
 void display() {
+    glClearColor(21 / 255.0, 239 / 255.0, 116/ 255.0, 0.81);
     glClear(GL_COLOR_BUFFER_BIT);
     glBegin(GL_POLYGON);
-    glVertex2f(-0.5, -0.5);
-    glVertex2f(-0.5, 0.5);
-    glVertex2f(0.5, 0.5);
-    glVertex2f(0.5, -0.5);
+        glVertex2f(0, 0);
+        glVertex2f(0, 1);
+        glVertex2f(1, 0.5);
+        glVertex2f(1, 1.5);
     glEnd();
     glFlush();
 }
-int main()
+int main(int argc, char ** argv)
 {
+    //Init Library
+    glutInit(&argc, argv);
+    //Setting Display mode
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    //Create Window for display
     glutCreateWindow("simple window");
-    glutDisplayFunc(display);
+    //Setting position to display
+    glutInitWindowPosition(0, 0);
+    //Setting window sizes
+    glutInitWindowSize(500, 500);
+    //Setup <display> as callback for display event
+    glutDisplayFunc(display); 
+    //Set OpenGL state (user-defined)
+    init();
+    //Create events loop
     glutMainLoop();
+
+    return 0;
 }
 
