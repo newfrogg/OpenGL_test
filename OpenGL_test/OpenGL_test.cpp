14d0f279b344fcd6f8c9fa795cf5212a338b5859
newfrogg <85973867+newfrogg@users.noreply.github.com>
2023-02-07 16:33:44 +0700

    parallelogram

--- OpenGL_test/OpenGL_test.cpp
+++ OpenGL_test/OpenGL_test.cpp
@@ -6,50 +6,24 @@
 
 using namespace std;
 void init() {
-    glColor3f(0, 0, 1);
-    glClearColor(1, 1, 1, 1);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    //glOrtho(left, right, bottom, top, near, far)
-    glOrtho(-5.0, 5.0, -5.0, 5.0, -1.0, 1.0);
     return;
 }
-void display() {
-
+void myDisplay() {
     glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(1.0f, 1.0f, 1.0f);
 
     glBegin(GL_POLYGON);
-        glVertex2f(-2.0, 0.0);
-        glVertex2f(-2.0, 2.0);
-        glVertex2f(0.0, 2.0);
-        glVertex2f(0.0, 0.0);
+        glVertex2f(0, 0);
+        glVertex2f(1, 0.5);
+        glVertex2f(0.5, 0);
+        glVertex2f(0.5, 0.5);
     glEnd();
-
-    glBegin(GL_POLYGON);
-        glVertex2f(0.0, -4.0);
-        glVertex2f(2.0, 0.0);
-        glVertex2f(4.0, -4.0);
-    glEnd();
-
     glFlush();
 }
 int main(int argc, char ** argv)
 {
-    //Init Library
-    glutInit(&argc, argv);
-    //Setting Display mode
-    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-    //Create Window for display
-    glutCreateWindow("simple window");
-    //Setting position to display
-    glutInitWindowPosition(0, 0);
-    //Setting window sizes
-    glutInitWindowSize(500, 500);
-    //Setup <display> as callback for display event
-    glutDisplayFunc(display); 
-    //Set OpenGL state (user-defined)
-    init();
-    //Create events loop
+    glutCreateWindow("Simple Square");
+    glutDisplayFunc(myDisplay);
     glutMainLoop();
 
     return 0;
